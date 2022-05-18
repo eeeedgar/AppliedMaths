b85c89bbbfab9370dcbaa5b10f9cc5aa3d0a991e
vekajp <belka.v.2002@gmail.com>
2022-05-18 10:45:06 +0300

    csr realisation, inversion, lu-decomposition

--- Lab-3/matrix.py
+++ Lab-3/matrix.py
@@ -1,16 +1,128 @@
+import numpy as np
+
+
 class csr_matrix:
     eps = 1e-5
-    data = []
-    indices = []
-    indptr = [1]
 
     def __init__(self, matrix):
-        for i in range(matrix.shape[0]):
+        self.n = matrix.shape[0]
+        self.m = matrix.shape[1]
+        self.data = []
+        self.indices = []
+        self.l = []
+        self.u = []
+        self.indptr = [1]
+        for i in range(self.n):
             count = 0
-            for j in range(matrix.shape[1]):
+            for j in range(self.m):
                 if matrix[i, j] != 0:
                     self.data.append(matrix[i, j])
                     self.indices.append(j)
                     count += 1
             self.indptr.append(self.indptr[i] + count)
 
+    def get(self, i, j):
+        start = self.indptr[i]
+        finish = self.indptr[i + 1]
+        for k in range(start, finish):
+            if self.indices[k - 1] == j:
+                return self.data[k - 1]
+        return 0
+
+    def set(self, i, j, value):
+        if value == 0:
+            return
+        start = self.indptr[i]
+        finish = self.indptr[i + 1]
+        for k in range(start, finish):
+            if self.indices[k - 1] == j:
+                self.data[k - 1] = value
+                return
+
+        self.indices.insert(finish - 1, j)
+        self.data.insert(finish - 1, value)
+
+        for k in range(i + 1, self.n + 1):
+            self.indptr[k] += 1
+
+    def print(self):
+        for i in range(self.n):
+            line = ""
+            for j in range(self.m):
+                line += str(self.get(i, j)) + " "
+            print(line)
+
+    def get_matrix(self):
+        result = np.zeros([self.n, self.m])
+        for i in range(self.n):
+            for j in range(self.m):
+                result[i, j] = self.get(i, j)
+        return result
+
+    def lu_decomposition(self):
+        if len(self.l) == 0 or len(self.u) == 0:
+            l_m = csr_matrix(np.zeros([self.n, self.m]))
+            u_m = csr_matrix(np.zeros([self.n, self.m]))
+
+            for i in range(self.n):
+                l_m.set(i, i, 1)
+                for j in range(i, self.n):
+                    s = 0
+                    for k in range(i):
+                        s += l_m.get(i, k) * u_m.get(k, j)
+                    u_m.set(i, j, self.get(i, j) - s)
+                for j in range(i + 1, self.n):
+                    s = 0
+                    for k in range(i):
+                        s += l_m.get(j, k) * u_m.get(k, i)
+                    l_m.set(j, i, (self.get(j, i) - s) / u_m.get(i, i))
+            self.l = l_m
+            self.u = u_m
+
+        return self.l, self.u
+
+    def transpose(self):
+        result = csr_matrix(np.zeros([self.m, self.n]))
+        for i in range(self.m):
+            for j in range(self.n):
+                result.set(i, j, self.get(j, i))
+        return result
+
+    def multiply(self, other):
+        result = csr_matrix(np.zeros([self.n, other.m]))
+        for i in range(self.n):
+            for j in range(other.m):
+                res = 0
+                for k in range(self.m):
+                    res += self.get(i, k) * other.get(k, j)
+                result.set(i, j, res)
+        return result
+
+    def inverse_upper_matrix(self):
+        result = csr_matrix(np.zeros([self.n, self.n]))
+        for i in range(self.n):
+            result.set(i, i, 1 / self.get(i, i))
+            for j in range(i + 1, self.n):
+                s = 0
+                for k in range(j):
+                    s -= result.get(i, k) * self.get(k, j)
+                result.set(i, j, s / self.get(j, j))
+        return result
+
+    def inverse_down_matrix(self):
+        matrix = self.transpose()
+        return matrix.inverse_upper_matrix().transpose()
+
+    def inverse_matrix(self):
+        lu = self.lu_decomposition()
+        l_inv = lu[0].inverse_down_matrix()
+        u_inv = lu[1].inverse_upper_matrix()
+        return u_inv.multiply(l_inv)
+
+    def det(self):
+        res = 1
+        lu = self.lu_decomposition()
+        u = lu[1]
+        for i in range(self.n):
+            res *= u.get(i, i)
+        return res
--- Lab-3/test.py
+++ Lab-3/test.py
@@ -3,6 +3,7 @@ import random as rnd
 import numpy as np
 import main as m
 import matrix_generation as mg
+import matrix as matr
 
 EPS = 1e-3
 
@@ -13,6 +14,7 @@ def equal_with_eps(first, second, eps):
     for i in range(first.shape[0]):
         for j in range(first.shape[1]):
             if abs(first[i, j] - second[i, j]) >= eps:
+                print(first[i, j], second[i, j])
                 return False
     return True
 
@@ -50,6 +52,90 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(equal_with_eps(inverse, inverse_with_np, EPS), True)
 
 
+class csr_test(unittest.TestCase):
+    def test_csr_init(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(generated_matrix)
+            mat = csr.get_matrix()
+            self.assertEqual(equal_with_eps(generated_matrix, mat, EPS), True)
+
+    def test_csr_insert_in_order(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(np.zeros([size, size]))
+            for j in range(size):
+                for k in range(size):
+                    if generated_matrix[j, k] != 0:
+                        csr.set(j, k, generated_matrix[j, k] - 1)
+                        csr.set(j, k, csr.get(j, k) + 1)
+            mat = csr.get_matrix()
+            self.assertEqual(equal_with_eps(generated_matrix, mat, EPS), True)
+
+    def test_csr_insert_out_of_order(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(np.zeros([size, size]))
+            pairs = [[0, 0]] * size ** 2
+            for j in range(size):
+                for k in range(size):
+                    pairs.insert(rnd.randint(0, size ** 2), [j, k])
+            for j in range(len(pairs)):
+                csr.set(pairs[j][0], pairs[j][1], generated_matrix[pairs[j][0], pairs[j][1]])
+            mat = csr.get_matrix()
+            self.assertEqual(equal_with_eps(generated_matrix, mat, EPS), True)
+
+    def test_lu_decomposition(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(generated_matrix)
+            lu = csr.lu_decomposition()
+            lu_multiplied = np.matmul(lu[0].get_matrix(), lu[1].get_matrix())
+            self.assertEqual(equal_with_eps(lu_multiplied, generated_matrix, EPS), True)
+
+    def test_multiply(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(generated_matrix)
+            lu = csr.lu_decomposition()
+            mult = lu[0].multiply(lu[1])
+            lu_multiplied = np.matmul(lu[0].get_matrix(), lu[1].get_matrix())
+            self.assertEqual(equal_with_eps(lu_multiplied, mult.get_matrix(), EPS), True)
+
+    def test_inverse_triangle_matrix(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            m1 = mg.generate_triangle_reversible_matrix(size, True)
+            m2 = mg.generate_triangle_reversible_matrix(size, False)
+            generated_upper_matrix = matr.csr_matrix(m1)
+            generated_down_matrix = matr.csr_matrix(m2)
+            inversion_upper = generated_upper_matrix.inverse_upper_matrix()
+            inversion_down = generated_down_matrix.inverse_down_matrix()
+            inversion_upper_np = np.linalg.inv(m1)
+            inversion_down_np = np.linalg.inv(m2)
+            self.assertEqual(equal_with_eps(inversion_upper.get_matrix(), inversion_upper_np, EPS), True)
+            self.assertEqual(equal_with_eps(inversion_down.get_matrix(), inversion_down_np, EPS), True)
+
+    def test_inverse_matrix(self):
+        for i in range(1000):
+            rnd.seed()
+            size = rnd.randint(3, 10)
+            generated_matrix = mg.generate_sparse_matrix(size)
+            csr = matr.csr_matrix(generated_matrix)
+            inverse = csr.inverse_matrix()
+            inverse_with_np = np.linalg.inv(generated_matrix)
+            self.assertEqual(equal_with_eps(inverse.get_matrix(), inverse_with_np, EPS), True)
 
 
 if __name__ == '__main__':
